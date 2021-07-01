911f387fcdbecb49b89a58d2cf474d8bf85499fc
dami1080 <dareakintoye@gmail.com>
2021-06-30 13:47:58 -1100

    create a javascript arrary of objects to store speakers

--- app.js
+++ app.js
@@ -13,3 +13,30 @@ hamburger.addEventListener('click', () => {
 closeIcon.addEventListener('click', () => {
   body.classList.remove('active');
 });
+
+const createSpeaker = (speaker) => {
+    const li = document.createElement('li');
+    li.setAttribute('class', 'speakers-list-item');
+    const img = document.createElement('img');
+    img.setAttribute('src', speaker.photoUrl);
+    img.setAttribute('class', 'image');
+    img.setAttribute('alt', 'image');
+    const div = document.createElement('div');
+    div.setAttribute('class', 'speakers-list-item__info');
+    const h2 = document.createElement('h2');
+    h2.setAttribute('class', 'speakers-list-item__heading');
+    h2.textContent = speaker.speakerName;
+    const h3 = document.createElement('h3');
+    h3.setAttribute('class', 'speakers-list-item__role');
+    h3.textContent = speaker.role;
+    const p = document.createElement('p');
+    p.setAttribute('class', 'speakers-list-item__text');
+    p.textContent = speaker.description;
+    li.appendChild(img);
+    div.appendChild(h2);
+    div.appendChild(h3);
+    div.appendChild(p);
+    li.appendChild(div);
+    return li;
+};
+  
